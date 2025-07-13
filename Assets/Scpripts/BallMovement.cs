0ed78ab85a10f500f3bea1431fb1876ada738992
Mark <markbojko85@gmail.com>
2025-07-13 18:56:15 +0300

    little code refactoring

--- Assets/Scpripts/BallMovement.cs
+++ Assets/Scpripts/BallMovement.cs
@@ -1,23 +1,33 @@
-using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    [SerializeField] private float _ballSpeed = 1f;
-    private Vector2 initialDirection;
+    [SerializeField] private float ballSpeed = 1f;
+    private Rigidbody2D _rb;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        initialDirection = new Vector2(1, 0).normalized;
-        rb.velocity = initialDirection * _ballSpeed;
+        _rb = GetComponent<Rigidbody2D>();
+        Vector2 initialDirection = new Vector2(1, 0).normalized;
+        _rb.velocity = initialDirection * ballSpeed;
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Platform"))
         {
-            Vector2 firstContact = collision.contacts[0].normal;
-            rb.velocity = Vector2.Reflect(rb.velocity.normalized,firstContact) * _ballSpeed;
+            Rigidbody2D platformRb = collision.gameObject.GetComponent<Rigidbody2D>();
+
+            // Базове відбиття від нормалі зіткнення
+            Vector2 normal = collision.contacts[0].normal;
+            Vector2 reflectedVelocity = Vector2.Reflect(_rb.velocity.normalized, normal);
+
+            // Додаємо вплив швидкості платформи по Y
+            float impactFactor = Mathf.Clamp(Mathf.Abs(platformRb.velocity.y) / 5f, 0.5f, 2f);
+            reflectedVelocity.y += platformRb.velocity.y * impactFactor;
+
+            // Нормалізуємо та встановлюємо швидкість
+            _rb.velocity = reflectedVelocity.normalized * ballSpeed;
         }
     }
 }
